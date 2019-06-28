98c854b756801d00625410e73b2011d5c4070aaf
bitprohib <tobias.blasinski@st.ovgu.de>
2019-06-28 14:10:08 +0200

    merge target.lua file

--- src/luaScripts/target.lua
+++ src/luaScripts/target.lua
@@ -1,6 +1,20 @@
 function sysCall_threadmain()
     local thisObjectHandle = sim.getObjectAssociatedWithScript(sim.handle_self)
-    local pathHandle = sim.getObjectHandle('Path0')
     local changePositionOnly = 1
-    sim.followPath(thisObjectHandle, pathHandle, changePositionOnly, 0, 0.01, 1)
+    local changeOrientationOnly = 2
+    local changePositionAndOrientation = 3
+    local pathHandle = sim.getObjectHandle('Path2')
+    sim.followPath(thisObjectHandle, pathHandle, changePositionOnly, 0, 0.2, 1)
+--    sub=simROS.subscribe('/Category', 'std_msgs/Int16', 'cubit_sort_callback')
+--    simROS.subscriberTreatUInt8ArrayAsString(sub) -- treat uint8 arrays as strings (much faster, tables/arrays are kind of slow in Lua)
 end
+
+--function cubit_sort_callback
+--    local category = msg.data
+--    print('Read this value as category: ' .. msg.data)
+    --local pathHandle = sim.getObjectHandle('Path' .. category)
+--    if msg.data and sim.readProximitySensor(proxSensor)>0 then
+--             sim.followPath(thisObjectHandle, pathHandle, changePositionOnly, 0, 0.25, 1)
+--    end 
+--end
+
