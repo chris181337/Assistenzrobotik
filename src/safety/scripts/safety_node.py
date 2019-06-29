fixed bug in KPI-visualisation - a bit hacky... still mistake in KPI-calculation

src/safety/scripts/safety_node.py
@@ -13,100 +13,6 @@ from sensor_msgs.msg import Image as ImageMsg
 from std_msgs.msg import Float32MultiArray as std_msg_f32_array
 from std_msgs.msg import Float32 as std_msg_f32
 
-#class ObjectClassification(object):
-#	def __init__(self):
-#
-#		self.pub = rospy.Publisher(
-#				'/Category', classific, queue_size=10) # (topicName, messageType, bufferSize)
-#		self.sub = rospy.Subscriber(
-#				"/Image", ImageMsg, self.callback)
-#		self.bridge = CvBridge()
-#
-#		rospack = rospkg.RosPack()
-#		self.model = tf.keras.models.load_model(rospack.get_path('vision') + '/src/arob_classification_model.h5')
-#		self.model._make_predict_function()
-#		self.model.summary()
-#		print(self.model.output)
-#
-#
-#	def classify_objects(self, image):
-#		""" Calculates the object classification neural network output
-#
-#		Args:
-#			image: np.array with shape [H, W, 3] and dtype np.uint8
-#				the image to detect objects in
-#
-#		Returns:
-#			probs: np.array with shape [NumDetections]
-#				probabilitiy of the detected object
-#		"""
-#
-#		image = np.array(Image.fromarray(image).resize((256, 256)))
-#		image = np.expand_dims(image, axis=0)
-#		return self.model.predict(image)
-#
-#
-#	def publish_classification(self, image_msg, probs):
-#		"""    Args:
-#			image_msg: sensor_msgs.msg.Image
-#				initial Image received from the camera gives timestamp to detection
-#			probs: np.array with shape [NumDetections]
-#				probabilitiy of the detected object
-#
-#		Returns:
-#			None
-#
-#		Side-Effects:
-#			Publishes classification_message.msg
-#		"""
-#
-#		#new msg-object
-#		classif = classific()
-#
-#		obj_max_probs = np.argmax(probs)
-#		max_probs = np.max(probs)
-#		if  max_probs <= 0.6:
-#			# no recognition 
-#			classif.obj_type = 3
-#			classif.prob = max_probs
-#			# class with highest probability 
-#		else:
-#			classif.obj_type = obj_max_probs
-#			classif.prob = max_probs
-#
-#		self.pub.publish(classif)
-#
-#
-#	def callback(self, image_msg):
-#		""" Called once a new image becomes available. Performs object
-#					detection and publishes results.
-#
-#		Args:
-#			image_msg: sensor_msgs.msg.Image
-#				camera image to perform object detection with
-#
-#		Returns:
-#			None
-#
-#		Side-Effects:
-#			Publishes robotto_msgs/Detections with contained objects
-#		"""
-#		print("Hello Image :)")
-#		try:
-#			# converting to numpy-array
-#			image = self.bridge.imgmsg_to_cv2(image_msg, "rgb8")
-#		except CvBridgeError as e:
-#			rospy.logerr(e)
-#			return
-#
-#		probs = self.classify_objects(image)
-#		self.publish_classification(image_msg, probs)
-
-
-##### Main #######
-
-#def publisher_func():
-
 publisher = ""
 
 def message_callback(msg):
@@ -116,9 +22,11 @@ def message_callback(msg):
 	print(minimum)
 	publisher.publish(minimum)
 
+######## MAIN #############
 
 def main():
 	rospy.init_node("safety_node")
+	print("[Safety_node] started")
 	global publisher
 	publisher = rospy.Publisher("/safety_level", std_msg_f32, queue_size=10) # (topicName, messageType, bufferSize)
 	rospy.Subscriber("/safety_sensor_data", std_msg_f32_array, message_callback)