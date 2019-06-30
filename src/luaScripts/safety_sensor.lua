5a55b75b9c2c7814821406ce8a06e69fce641ac2
Tom Heimbrodt <heimbrodt@posteo.de>
2019-06-30 16:17:48 +0200

    temporary changes

--- src/luaScripts/safety_sensor.lua
+++ src/luaScripts/safety_sensor.lua
@@ -63,79 +63,79 @@ function sysCall_sensing()
 
     measuredData={}
     
-    --if notFirstHere then
-    --    -- We skip the very first reading
-    --    sim.addDrawingObjectItem(lines,nil)
-    --    showLines=sim.getScriptSimulationParameter(sim.handle_self,'showLaserSegments')
-    --    r,t1,u1=sim.readVisionSensor(visionSensor1Handle)
-    --    r,t2,u2=sim.readVisionSensor(visionSensor2Handle)
+    if notFirstHere then
+        -- We skip the very first reading
+        sim.addDrawingObjectItem(lines,nil)
+        showLines=sim.getScriptSimulationParameter(sim.handle_self,'showLaserSegments')
+        r,t1,u1=sim.readVisionSensor(visionSensor1Handle)
+        r,t2,u2=sim.readVisionSensor(visionSensor2Handle)
     
-    --    m1=sim.getObjectMatrix(visionSensor1Handle,-1)
-    --    m01=simGetInvertedMatrix(sim.getObjectMatrix(sensorRefHandle,-1))
-    --    m01=sim.multiplyMatrices(m01,m1)
-    --    m2=sim.getObjectMatrix(visionSensor2Handle,-1)
-    --    m02=simGetInvertedMatrix(sim.getObjectMatrix(sensorRefHandle,-1))
-    --    m02=sim.multiplyMatrices(m02,m2)
-    --    if u1 then
-    --        p={0,0,0}
-    --        p=sim.multiplyVector(m1,p)
-    --        t={p[1],p[2],p[3],0,0,0}
-    --        for j=0,u1[2]-1,1 do
-    --            for i=0,u1[1]-1,1 do
-    --                w=2+4*(j*u1[1]+i)
-    --                v1=u1[w+1]
-    --                v2=u1[w+2]
-    --                v3=u1[w+3]
-    --                v4=u1[w+4]
-    --                if (v4<maxScanDistance_) then
-    --                    p={v1,v2,v3}
-    --                    p=sim.multiplyVector(m01,p)
-    --                    table.insert(measuredData,p[1])
-    --                    table.insert(measuredData,p[2])
-    --                    table.insert(measuredData,p[3])
-    --                end
-    --                if showLines then
-    --                    p={v1,v2,v3}
-    --                    p=sim.multiplyVector(m1,p)
-    --                    t[4]=p[1]
-    --                    t[5]=p[2]
-    --                    t[6]=p[3]
-    --                    sim.addDrawingObjectItem(lines,t)
-    --                end
-    --            end
-    --        end
-    --    end
-    --    if u2 then
-    --        p={0,0,0}
-    --        p=sim.multiplyVector(m2,p)
-    --        t={p[1],p[2],p[3],0,0,0}
-    --        for j=0,u2[2]-1,1 do
-    --            for i=0,u2[1]-1,1 do
-    --                w=2+4*(j*u2[1]+i)
-    --                v1=u2[w+1]
-    --                v2=u2[w+2]
-    --                v3=u2[w+3]
-    --                v4=u2[w+4]
-    --                if (v4<maxScanDistance_) then
-    --                    p={v1,v2,v3}
-    --                    p=sim.multiplyVector(m02,p)
-    --                    table.insert(measuredData,p[1])
-    --                    table.insert(measuredData,p[2])
-    --                    table.insert(measuredData,p[3])
-    --                end
-    --                if showLines then
-    --                    p={v1,v2,v3}
-    --                    p=sim.multiplyVector(m2,p)
-    --                    t[4]=p[1]
-    --                    t[5]=p[2]
-    --                    t[6]=p[3]
-    --                    sim.addDrawingObjectItem(lines,t)
-    --                end
-    --            end
-    --        end
-    --    end
-    --end
-    --notFirstHere=true
+        m1=sim.getObjectMatrix(visionSensor1Handle,-1)
+        m01=simGetInvertedMatrix(sim.getObjectMatrix(sensorRefHandle,-1))
+        m01=sim.multiplyMatrices(m01,m1)
+        m2=sim.getObjectMatrix(visionSensor2Handle,-1)
+        m02=simGetInvertedMatrix(sim.getObjectMatrix(sensorRefHandle,-1))
+        m02=sim.multiplyMatrices(m02,m2)
+        if u1 then
+            p={0,0,0}
+            p=sim.multiplyVector(m1,p)
+            t={p[1],p[2],p[3],0,0,0}
+            for j=0,u1[2]-1,1 do
+                for i=0,u1[1]-1,1 do
+                    w=2+4*(j*u1[1]+i)
+                    v1=u1[w+1]
+                    v2=u1[w+2]
+                    v3=u1[w+3]
+                    v4=u1[w+4]
+                    if (v4<maxScanDistance_) then
+                        p={v1,v2,v3}
+                        p=sim.multiplyVector(m01,p)
+                        table.insert(measuredData,p[1])
+                        table.insert(measuredData,p[2])
+                        table.insert(measuredData,p[3])
+                    end
+                    if showLines then
+                        p={v1,v2,v3}
+                        p=sim.multiplyVector(m1,p)
+                        t[4]=p[1]
+                        t[5]=p[2]
+                        t[6]=p[3]
+                        sim.addDrawingObjectItem(lines,t)
+                    end
+                end
+            end
+        end
+        if u2 then
+            p={0,0,0}
+            p=sim.multiplyVector(m2,p)
+            t={p[1],p[2],p[3],0,0,0}
+            for j=0,u2[2]-1,1 do
+                for i=0,u2[1]-1,1 do
+                    w=2+4*(j*u2[1]+i)
+                    v1=u2[w+1]
+                    v2=u2[w+2]
+                    v3=u2[w+3]
+                    v4=u2[w+4]
+                    if (v4<maxScanDistance_) then
+                        p={v1,v2,v3}
+                        p=sim.multiplyVector(m02,p)
+                        table.insert(measuredData,p[1])
+                        table.insert(measuredData,p[2])
+                        table.insert(measuredData,p[3])
+                    end
+                    if showLines then
+                        p={v1,v2,v3}
+                        p=sim.multiplyVector(m2,p)
+                        t[4]=p[1]
+                        t[5]=p[2]
+                        t[6]=p[3]
+                        sim.addDrawingObjectItem(lines,t)
+                    end
+                end
+            end
+        end
+    end
+    notFirstHere=true
     
     -- measuredData now contains all the points that are closer than the sensor range
     -- For each point there is the x, y and z coordinate (i.e. 3 number for each point)
