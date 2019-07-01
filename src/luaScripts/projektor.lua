b29bab48878ac6b09da39c75e6cee30c1651fcba
chris181337 <chris181337>
2019-07-01 18:02:08 +0200

    beautify

--- src/luaScripts/projektor.lua
+++ src/luaScripts/projektor.lua
@@ -2,8 +2,7 @@ function sysCall_init()
    sub1=simROS.subscribe('/qbit_ready', 'std_msgs/Bool', 'qbitready_callback')--subscribe: chris qbit ready
    sub2=simROS.subscribe('/Category', 'std_msgs/Int16', 'cubit_sort_callback')--subscribe: nele qbit category
    sub3=simROS.subscribe('/safety_level', 'std_msgs/Float32','safety_callback')--subscribe: tom security speed
-   --simROS.subscriberTreatUInt8ArrayAsString(sub) -- treat uint8 arrays as strings (much faster, tables/arrays are kind of slow in Lua)
---initial Signale
+  --initial Signale
 	sim.setIntegerSignal("safety_signal",0)   --nich safe
 	sim.setIntegerSignal("category_signal", 4)--nix erkannt
 	sim.setIntegerSignal("ready_signal", 0)   --nich ready
